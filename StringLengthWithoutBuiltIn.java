47bea99f0de58918da6fce25fc806e7120e8a7ee
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-07 14:12:26 +0530

    [Praveen] ADD: String Length Without BuiltIn

--- StringLengthWithoutBuiltIn.java
+++ StringLengthWithoutBuiltIn.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class StringLengthWithoutBuiltIn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a string: "); 
         String text = scanner.next();
 
         int length1 = findLength(text);
